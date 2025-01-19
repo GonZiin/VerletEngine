5722c4b152594e84730474423f4ff9c5b4d6ef2b
GonZiin <goncalogomespessoal@outlook.pt>
2025-01-19 21:05:17 +0000

    Build update

--- build/CMakeFiles/VerletEngine.dir/src/main.cpp.o.d
+++ build/CMakeFiles/VerletEngine.dir/src/main.cpp.o.d
@@ -1,11 +1,12 @@
 CMakeFiles/VerletEngine.dir/src/main.cpp.o: \
  /home/gomes/c++/sfml/engine_fisica/src/main.cpp \
- /usr/include/stdc-predef.h /usr/include/SFML/Graphics.hpp \
- /usr/include/SFML/Window.hpp /usr/include/SFML/System.hpp \
- /usr/include/SFML/Config.hpp /usr/include/SFML/System/Clock.hpp \
- /usr/include/SFML/System/Export.hpp /usr/include/SFML/System/Time.hpp \
- /usr/include/SFML/System/Err.hpp /usr/include/c++/14/ostream \
- /usr/include/c++/14/bits/requires_hosted.h \
+ /usr/include/stdc-predef.h \
+ /home/gomes/c++/sfml/engine_fisica/src/renderer.hpp \
+ /usr/include/SFML/Graphics.hpp /usr/include/SFML/Window.hpp \
+ /usr/include/SFML/System.hpp /usr/include/SFML/Config.hpp \
+ /usr/include/SFML/System/Clock.hpp /usr/include/SFML/System/Export.hpp \
+ /usr/include/SFML/System/Time.hpp /usr/include/SFML/System/Err.hpp \
+ /usr/include/c++/14/ostream /usr/include/c++/14/bits/requires_hosted.h \
  /usr/include/c++/14/x86_64-redhat-linux/bits/c++config.h \
  /usr/include/bits/wordsize.h \
  /usr/include/c++/14/x86_64-redhat-linux/bits/os_defines.h \
