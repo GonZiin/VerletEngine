04add5282d7b1019cf3c3c4a5b4d4f5d5a175e4c
GonZiin <goncalogomespessoal@outlook.pt>
2025-01-19 20:50:59 +0000

    Settings Changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,34 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window/Context.hpp>
+#include <SFML/Window/ContextSettings.hpp>
 #include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <SFML/Window/VideoMode.hpp>
 #include <SFML/Window/Window.hpp>
+#include <cstdint>
 #include <iostream>
 
-const unsigned int altura = 1000;
-const unsigned int largura = 1000;
-
 int main(int argc, char *argv[]) {
+
+  // Window settings
+  constexpr int32_t window_width = 1000;
+  constexpr int32_t window_height = 1000;
+
   // Window creation
-  sf::RenderWindow window(sf::VideoMode(largura, altura), "VerletEngine");
+  sf::ContextSettings settings;
+  settings.antialiasingLevel = 1;
+  sf::RenderWindow window(sf::VideoMode(window_width, window_height),
+                          "VerletEngine");
+  const uint32_t frame_rate = 60;
+  window.setFramerateLimit(frame_rate);
 
   // Window Main Loop
   while (window.isOpen()) {
     sf::Event event; // Event creation
     while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed)
+      if (event.type == sf::Event::Closed ||
+          sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
         window.close();
     }
 
