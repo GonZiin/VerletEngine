e078dc2dd95364c34b364af74f952e78957b0dee
GonZiin <goncalogomespessoal@outlook.pt>
2025-01-19 20:19:38 +0000

    Window Creation

---
+++ build/CMakeFiles/VerletEngine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VerletEngine.dir/src/main.cpp.o"
+  "CMakeFiles/VerletEngine.dir/src/main.cpp.o.d"
+  "VerletEngine"
+  "VerletEngine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VerletEngine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
